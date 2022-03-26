e2a0247b833d33a5c9b2823004f6e589486dd03c
Oscar <a127309512@gmail.com>
2022-03-26 16:41:53 +0800

    NgClass
    NgClass練習

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock|undefined;
+  public stockClasses: any;
 
   constructor() { }
 
   ngOnInit(): void {
     this.stock = new Stock('Test Stock Company','TSC',85,80);
+    let diff = (this.stock.price/this.stock.previousPrice)-1;
+    let largeChange = Math.abs(diff) > 0.01;
+
+    this.stockClasses = {
+      "positive" : this.stock.isPositiveChange(),
+      "negative" : !this.stock.isPositiveChange(),
+      "large-change" : largeChange,
+      "small-change" : !largeChange
+    }
+
   }
 
   toggleFavorite(event: any){
