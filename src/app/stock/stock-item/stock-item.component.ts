0d1d64a2cd915e0a3e38dcfb537cb299e3bae888
Oscar <a127309512@gmail.com>
2022-03-26 16:47:55 +0800

    NgStyle
    NgStyle練習

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,11 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock|undefined;
+  public stockStyles: any;
 
   constructor() { }
 
   ngOnInit(): void {
     this.stock = new Stock('Test Stock Company','TSC',85,80);
+    let largeChange = Math.abs((this.stock.price/this.stock.previousPrice)-1) > 0.01;
+    this.stockStyles = {
+      "color" : this.stock.isPositiveChange() ? 'green' : 'red',
+      "font-size" : largeChange ? '1.2em' : '.8em'
+    }
   }
 
   toggleFavorite(event: any){
